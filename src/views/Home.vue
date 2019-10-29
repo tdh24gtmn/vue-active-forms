5fe579ee720a02670562a1a1f4315510df2b647d
tdh2f4gtmn <46089697+tdh24gtmn@users.noreply.github.com>
2019-10-29 20:02:46 +0200

    add vuetify plugin in vue ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <BaseUserCard />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+import BaseUserCard from "@/components/BaseUserCard.vue";
+
+// import BaseAside from "@/components/BaseAside.vue";
+// import BaseMain from "@/components/BaseMain.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    // HelloWorld,
+    // BaseAside,
+    // BaseMain,
+    BaseUserCard
   }
-}
+};
 </script>
+<style>
+.home {
+  /* margin: 150px 100px 150px 66px; */
+}
+</style>
